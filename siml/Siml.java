44f2eda5f14a016a14cbf07d904e3c65a91dd18e
LYXcode <1367527584@qq.com>
2021-04-21 22:55:28 +0800

    new name

--- siml/Siml.java
+++ siml/Siml.java
@@ -1,8 +1,11 @@
 package siml;
 
 import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 
 public class Siml {
     public static void main(String[] args) throws IOException {
@@ -22,11 +25,28 @@ public class Siml {
     }
 
     private static void runFile(String filePath) throws IOException {
-        System.out.println(filePath);
+      
         byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        String source = new String(bytes, Charset.defaultCharset());
+        run(source);
+        
     }
 
-    private static void runPromt() {
+    private static void runPromt() throws IOException {
+        InputStreamReader inputStream = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStream);
+        for(;;){
+            System.out.println("> ");
+            String line = bufferedReader.readLine();
+            if(line == null){
+                break;
+            }
+            run(line);
+        }
 
+    }
+
+    private static void run(String source){
+        
     }
 }
